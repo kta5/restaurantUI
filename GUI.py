38b75dc4337b2f5920b0db6b161cc316874ecd1b
Justin Lok <32407493+justinlok@users.noreply.github.com>
2018-11-28 10:18:47 -0800

    moved tk to class member

--- GUI.py
+++ GUI.py
@@ -1,17 +1,18 @@
 import tkinter
 
 
-
 class GUI():
 
+    top = tkinter.Tk()
+
     def __init__(self):
-        top = tkinter.Tk()
         # Code to add widgets will go here...
-        top.geometry("500x500")
+        self.top.geometry("500x500")
         # Employee Login Option
-        w = tkinter.Button(top, text = "Employee Login", command=self.Login)
+        w = tkinter.Button(self.top, text = "Employee Login", command=self.Login)
         w.place(x = 50, y = 50)
-        top.mainloop()
+        self.top.update_idletasks()
+        self.top.update()
 
     def Login(self):
         msg = tkinter.messagebox.showInfo("Login Prompt", "Please enter your Username and Password")
