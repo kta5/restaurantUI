0b27c3437fc25b0c7985ddcc5076c91bd64e1be3
Justin Lok <32407493+justinlok@users.noreply.github.com>
2018-11-28 10:24:36 -0800

    gui main loop

--- GUI.py
+++ GUI.py
@@ -4,6 +4,7 @@ import tkinter
 class GUI():
 
     top = tkinter.Tk()
+    cuser = None
 
     def __init__(self):
         # Code to add widgets will go here...
@@ -11,17 +12,19 @@ class GUI():
         # Employee Login Option
         w = tkinter.Button(self.top, text = "Employee Login", command=self.Login)
         w.place(x = 50, y = 50)
-        self.top.update_idletasks()
-        self.top.update()
+        self.main()
 
     def Login(self):
         msg = tkinter.messagebox.showInfo("Login Prompt", "Please enter your Username and Password")
 
+    def main(self):
+        while True:
+            if self.cuser is not None:
+                self.Login()
 
-
-
-
-
+            self.top.update_idletasks()
+            self.top.update()
+            pass
 
 
 
