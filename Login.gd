a6d870667763f40e94735212738e952193aed019
Justin Lok <32407493+justinlok@users.noreply.github.com>
2018-11-30 13:31:18 -0800

    db connection

--- Login.gd
+++ Login.gd
@@ -3,45 +3,29 @@ extends Node
 # SQLite module
 const SQLite = preload("res://lib/gdsqlite.gdns");
 
+# Variables
+var current_user = null
+var entrybox_username = null
+var entrybox_password = null
+
+
 func _ready():
 	# Create gdsqlite instance
-	var db = SQLite.new();
-	
-	# Open database
-	if (!db.open_db("res://godot.sql")):
-		print("Cannot open database.");
-		return;
-	
-	var query = "";
-	var result = null;
-	
-	# Create table
-	query = "CREATE TABLE IF NOT EXISTS users (";
-	query += "id integer PRIMARY KEY,";
-	query += "first_name text NOT NULL,";
-	query += "last_name text NOT NULL,";
-	query += "email text NOT NULL";
-	query += ");";
-	result = db.query(query);
-	
-	# Fetch rows
-	query = "SELECT * FROM users;";
-	result = db.fetch_array(query);
-	
-	if (!result || result.size() <= 0):
-		# Insert new row
-		query = "INSERT INTO users (first_name, last_name, email) VALUES ('godot', 'engine', 'user@users.org');";
-		result = db.query(query);
-		
-		if (!result):
-			print("Cannot insert data!");
-		else:
-			print("Data inserted into table.");
-	
+	var db = SQLite.new()
+
+		# Open database
+	if (!db.open_db("res://restaurant.db")):
+		print("Cannot open database.")
+		return
 	else:
-		# Print rows
-		for i in result:
-			print(i);
+		print("Connected!")
+		
 	
-	# Close database
-	db.close();
+	pass
+
+func login():
+		# Get item list from db
+	var result = db.fetch_array("SELECT * from employees")
+	#if (not pots or pots.empty()):
+	#	return;
+	pass
