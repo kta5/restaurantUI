c85da182cb86fe9c89ce71d8d575c15259442c55
Justin Lok <32407493+justinlok@users.noreply.github.com>
2018-12-04 12:24:25 -0800

    main menu tscn

---
+++ scripts/mainmenu.gd
@@ -0,0 +1,26 @@
+extends Node
+
+
+var root_script = null
+
+func _ready():
+	root_script = get_tree().get_root().get_node("dbconnection")
+	displaybuttons()
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+func displaybuttons():
+	if root_script.user_group == "owner":
+	
+		pass
+	elif root_script.user_group == "cashier":
+	
+		pass
+	elif root_script.user_group == "chef":
+	
+		pass
+	pass
